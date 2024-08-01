fb6d519b35bee96f65972761db85283038c8021f
KimDongWoo <dw81770@gmail.com>
2024-08-01 10:16:35 +0900

    0731

--- final/node-admin-app/app.js
+++ final/node-admin-app/app.js
@@ -1,26 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 //dotenv를 이용한 환경변수 설정값 로딩처리
-require('dotenv').config();
+require("dotenv").config();
 
-var expressLayouts = require('express-ejs-layouts');
+var expressLayouts = require("express-ejs-layouts");
 
+//ORM DB연결객체 sequelize 참조하기
+var sequelize = require("./models/index.js").sequelize;
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-//ORM DB연결객체 sequelize 참조하기 
-var sequelize = require('./models/index.js').sequelize;
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var adminRouter = require('./routes/admin');
+var adminRouter = require("./routes/admin");
 // var memberRouter = require('./routes/member');
 // var channelRouter = require('./routes/channel');
-var articleRouter = require('./routes/article');
+var articleRouter = require("./routes/article");
 // var messageRouter = require('./routes/message');
 
 var app = express();
@@ -29,45 +27,44 @@ var app = express();
 sequelize.sync();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 //레이아웃 설정
-app.set('layout', 'layout');
+app.set("layout", "layout");
 app.set("layout extractScripts", true);
 app.set("layout extractStyles", true);
 app.set("layout extractMetas", true);
 app.use(expressLayouts);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use('/admin', adminRouter);
+app.use("/admin", adminRouter);
 // app.use('/member', memberRouter);
 // app.use('/channel', channelRouter);
-app.use('/article', articleRouter);
+app.use("/article", articleRouter);
 // app.use('/message', messageRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- final/node-chat-app/app.js
+++ final/node-chat-app/app.js
@@ -1,41 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//RESTful API 서비스 CORS 이슈해결을 위한 cors 패키지 참조하기
+const cors = require("cors");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//모든 웹사이트/모바일 프론트에서 RESTAPI를 접근할수 있게 허락함
+app.use(cors());
 
-app.use(logger('dev'));
+//특정 도메인주소만 허가
+// app.use(
+//   cors({
+//     methods: ["GET", "POST", "DELETE", "OPTIONS"],
+//     origin: ["http://localhost:3000", "https://naver.com"],
+//   })
+// );
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
